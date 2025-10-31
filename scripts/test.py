b4fc868da88ed27f7eeb7cbfb3d6078d58dc8bb9
Daniel Abanto Graffman <abanto@stud.uni-heidelberg.de>
2025-10-31 20:07:04 +0100

    final push

--- scripts/test.py
+++ scripts/test.py
@@ -2,6 +2,7 @@ import geopandas as gpd
 from shapely.geometry import box
 from spatial import RandomPoints
 
+#test if the polygon crs is set to 4326
 
 def test_polygon_crs_is_4326():
     heidelberg_bbox = (8.598690, 49.339503, 8.755589, 49.456632)
@@ -11,6 +12,7 @@ def test_polygon_crs_is_4326():
 
     assert rp.polygon.crs.to_string() == "EPSG:4326"
 
+#test if the desired amount of random points is created
 
 def test_random_points_count():
     heidelberg_bbox = (8.598690, 49.339503, 8.755589, 49.456632)
@@ -21,6 +23,7 @@ def test_random_points_count():
 
     assert len(rp.pnts_in_poly) == 50
 
+#test if the random points are within the geometry
 
 def test_random_points_within_polygon():
     heidelberg_bbox = (8.598690, 49.339503, 8.755589, 49.456632)
@@ -32,7 +35,7 @@ def test_random_points_within_polygon():
     for point in rp.pnts_in_poly.geometry:
         assert heidelberg_polygon_gdf.contains(point).any()
 
-
+#test if the required columns are there
 def test_sample_df_columns():
     heidelberg_bbox = (8.598690, 49.339503, 8.755589, 49.456632)
     polygon = box(*heidelberg_bbox)
@@ -44,7 +47,7 @@ def test_sample_df_columns():
 
     assert set(rp.df1_tail.columns) == expected_columns
 
-
+#test if the function does not return negative values
 def test_compute_distance_non_negative():
     heidelberg_bbox = (8.598690, 49.339503, 8.755589, 49.456632)
     polygon = box(*heidelberg_bbox)
